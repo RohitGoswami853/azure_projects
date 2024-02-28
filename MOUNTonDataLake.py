6d927b3a3c4cc536c1c753b70e013b1853cbc44f
Rohit Goswami <113711749+RohitGoswami853@users.noreply.github.com>
2024-02-28 22:53:55 +0530

    Add files via upload

---
+++ MOUNTonDataLake.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+configs = {
+  "fs.azure.account.auth.type": "CustomAccessToken",
+  "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
+}
+
+# Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://raw@projectss.dfs.core.windows.net/",
+  mount_point = "/mnt/raw",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+configs = {
+  "fs.azure.account.auth.type": "CustomAccessToken",
+  "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
+}
+
+# Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://transform@projectss.dfs.core.windows.net/",
+  mount_point = "/mnt/transform",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+
